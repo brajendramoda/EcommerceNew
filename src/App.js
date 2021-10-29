021f2fa2ce981e7057151cf53107fefdef3a5cdd
brajendramoda <64329480+brajendramoda@users.noreply.github.com>
2021-10-30 00:02:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./homepage";
+
+function App() {
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
